887f0f11aff484964e4e133b6e0132fbc4889790
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-11 17:34:07 +0000

    Implement login form validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em { float: right; color: #E05C65; padding-left:10px; }
+  `]
 })
 
 export class LoginComponent {
   userName
   password
+  mouseoverLogin
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
 
   }
   login(formValues) {
     this.authService.loginUser(formValues.userName, formValues.password)
+    this.router.navigate(['events'])
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
   }
 }
