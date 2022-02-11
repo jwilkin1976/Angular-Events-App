51fce2167210bf8440846888a4b7043444f5b6c2
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-11 16:13:54 +0000

    Implement data models for type safety

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '.';
 
 declare let toastr // Enusre typescript is aware of 3rd party library
 @Component({
@@ -18,7 +19,7 @@ declare let toastr // Enusre typescript is aware of 3rd party library
     `
 })
 export class EventsListComponent implements OnInit {
-    events: any
+    events: IEvent[]
 
     constructor(private eventService: EventService,
       private toastr: ToastrService,
