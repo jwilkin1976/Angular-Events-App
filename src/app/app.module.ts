245c11a59681162ed09ef04719285fc2e3342941
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-11 15:10:59 +0000

    Implement canActivate & canDeactivate route guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from 'src/routes';
 import { ToastrService } from './common/toastr.service';
-
+import { Error404Component } from './errors/404.component';
 import { EventsAppComponent } from './events-app.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
@@ -23,9 +24,24 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     EventDetailsComponent,
     NavBarComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './app/errors/404.component';
 import { CreateEventComponent } from './app/events/create-event.component';
 import { EventDetailsComponent } from './app/events/event-details/event-details.component';
+import { EventRouteActivator } from './app/events/event-details/event-route-activator.service';
 import { EventsListComponent } from './app/events/events-list.component';
 
 // We need to ensure our routes are in the correct order otherwise we may end up matching an unexpected route pattern
 export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent },
-  { path: 'events/new', component: CreateEventComponent},
-  { path: 'events/:id', component: EventDetailsComponent},
+  { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
+  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
+  { path: '404', component: Error404Component},
   { path: '', redirectTo: '/events', pathMatch: 'full' },
 
 ]
