0f12bb9d9d2e9bd0688a3e0df53ab5b3de50a0be
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-12 15:34:25 +0000

    Change collapsible well to use multiple content projection

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -4,15 +4,16 @@ import { Component, Input } from '@angular/core';
   selector: 'collapsible-well',
   template: `
   <div (click)="toggleContent()" class="well pointable">
-    <h4 class="well-title">{{title}}</h4>
+    <h4>
+      <ng-content select="[well-title]"></ng-content>
+    </h4>
     <!-- ng-content will include relevant content -->
-    <ng-content *ngIf="visible"></ng-content>
+    <ng-content *ngIf="visible" select="[well-body]"></ng-content>
   </div>
   `
 })
 
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = true;
 
   toggleContent() {
