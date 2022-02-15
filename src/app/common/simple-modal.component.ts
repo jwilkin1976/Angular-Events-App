af408757b5dde56963ed3d12f5ff0c3d5c0cc729
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-15 16:10:04 +0000

    Enhance modal component

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,30 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Inject, Input, ViewChild } from '@angular/core';
+import { JQ_TOKEN } from './jquery.service';
 @Component({
   selector: 'simple-modal',
   template: `
-  <div id="{{elementId}}" class="modal fade" tabindex="-1">
+  <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
     <div class="modal-dialog">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">
-          <span>&times;</span>
-        </button>
-        <h4 class="modal-title">{{title}}</h4>
-      </div>
-      <div class="modal-body">
-        <ng-content></ng-content>
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">
+            <span>&times;</span>
+          </button>
+          <h4 class="modal-title">{{title}}</h4>
+        </div>
+        <div class="modal-body" (click)="closeModal()">
+          <ng-content></ng-content>
+        </div>
       </div>
     </div>
   </div>
   `,
   styles: [`
-    .modal-body {height: 250px; overflow-y: scroll; background-color: #4e5d6c; }
+    .modal-body {height: 250px; overflow-y: scroll; }
   `]
 })
 
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @Input() closeOnBodyClick: string;
 
-  constructor() { }
+  @ViewChild('modalcontainer') containerEl: ElementRef;
+
+  constructor(@Inject(JQ_TOKEN) private $: any) {
+
+  }
+
+  closeModal() {
+    if (this.closeOnBodyClick.toLocaleLowerCase() === "true") {
+      this.$(this.containerEl.nativeElement).modal('hide');
+    }
+  }
 }
