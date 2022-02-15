cbd4b1ea8b4edd1f45804e9c7f8323e24e2203a7
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-15 13:12:35 +0000

    Add session level filtering

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { ISpeed } from 'selenium-webdriver';
 import { ISession } from '..';
 
 @Component({
@@ -6,8 +8,27 @@ import { ISession } from '..';
   templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent{
-  @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges{
+  @Input() sessions:ISession[];
+  @Input() filterBy:string;
+  visibleSessions: ISession[] = [];
 
   constructor() { }
+
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter: string) {
+    if (filter === 'all') {
+      // Use slice to create a new array containing all of the session items
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
+    }
+  }
 }
