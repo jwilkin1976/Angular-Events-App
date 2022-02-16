b8ad09a41bd2870189b6875f61c7a580f352fa1e
jwilkin1976 <89321791+jwilkin1976@users.noreply.github.com>
2022-02-16 15:05:56 +0000

    Switch to using a local server for data

--- src/routes.ts
+++ src/routes.ts
@@ -3,17 +3,17 @@ import { Error404Component } from './app/errors/404.component';
 import {
   CreateEventComponent,
   EventDetailsComponent,
-  EventRouteActivator,
   EventListResolver,
   EventsListComponent,
-  CreateSessionComponent
+  CreateSessionComponent,
+  EventResolver
 } from './app/events/index';
 
 // We need to ensure our routes are in the correct order otherwise we may end up matching an unexpected route pattern
 export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
   { path: 'events', component: EventsListComponent, resolve: {events: EventListResolver} },
-  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
+  { path: 'events/:id', component: EventDetailsComponent, resolve: {event: EventResolver} },
   { path: 'events/session/new', component: CreateSessionComponent },
   { path: '404', component: Error404Component},
   { path: '', redirectTo: '/events', pathMatch: 'full' },
